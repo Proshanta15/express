49a5d340a6d85ce4638def450a8c37139694972e
Proshanta15 <roy15-6997@diu.edu.bd>
2026-04-22 09:07:36 +0600

    express

--- index.js
+++ index.js
@@ -1,39 +1,19 @@
 import express from "express";
+import morgan from "morgan";
 
 const app = express();
+app.use(morgan("dev"));
 
-// function ageCheck(req, res, next) {
-//   if (!req.query.age || req.query.age < 18) {
-//     // Check if age is provided and is at least 18
-//     return res.send("You must be at least 18 years old to access this page.");
-//   } else {
-//     next();
-//   }
-// }
-// app.use(ageCheck);
-
-//ip address
-function ipCheck(req, res, next) {
-  const ip = req.socket.remoteAddress;
-  console.log(ip);
-  if (ip.includes("192.168.0.185")) {
-    return res.send("Access denied: Localhost IP address is not allowed.");
-  } else {
-    next();
-  }
-}
-app.use(ipCheck);
+// Create directory and file
 
 app.get("/", (req, res) => {
   res.send("Hello World");
 });
 
-app.get("/login", (req, res) => {
-  res.send("Login Page");
-});
-
-app.get("/admin", (req, res) => {
-  res.send("Admin Page");
+app.get("/wait", (req, res) => {
+  setTimeout(() => {
+    res.send("Waited for 1 second");
+  }, 1000);
 });
 
 app.listen(3000);
