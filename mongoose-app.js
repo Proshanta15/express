0ff533f36f96004ce73e070d0ab8e21338e60a92
Proshanta15 <roy15-6997@diu.edu.bd>
2026-04-28 23:22:06 +0600

    express

--- mongoose-app.js
+++ mongoose-app.js
@@ -1,8 +1,11 @@
+import cors from "cors";
 import express from "express";
 import mongoose from "mongoose";
 import studentModel from "./model/studentModel.js";
+
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 
 await mongoose
@@ -19,13 +22,13 @@ app.get("/", async (req, res) => {
   res.send(studentData);
 });
 
-app.post('/save', async(req, res) =>{
+app.post("/save", async (req, res) => {
   const result = req.body;
   const { name, age, email } = req.body;
-  if(!req.body || !name || !age || !email){
-     res.send({
+  if (!req.body || !name || !age || !email) {
+    res.send({
       message: "All fields are required",
-      success: false
+      success: false,
     });
     return false;
   }
@@ -33,29 +36,29 @@ app.post('/save', async(req, res) =>{
   res.send({
     message: "Data saved successfully",
     success: true,
-    data: studentData
+    data: studentData,
   });
 });
 
-app.put('/update/:id', async(req, res) =>{
+app.put("/update/:id", async (req, res) => {
   console.log(req.body);
   const id = req.params.id;
   console.log(id);
   const studentData = await studentModel.findByIdAndUpdate(id, { ...req.body });
-  
+
   res.send({
     message: "Update API is working",
-    success: true
-  })
+    success: true,
+  });
 });
 
-app.delete('/delete/:id', async(req, res) =>{
+app.delete("/delete/:id", async (req, res) => {
   const id = req.params.id;
   await studentModel.findByIdAndDelete(id);
   res.send({
     message: "Delete API is working",
-    success: true
-  })
+    success: true,
+  });
 });
 
 app.listen(3000, () => {
